08d3e29543c2a7f59d594484e0868444435de4c1
Simon Planinschek <planinscheksimon@outlook.com>
2020-10-07 08:42:56 +0200

    Added utilities functions for handling cookies and DOM verification

---
+++ src/utilities.ts
@@ -0,0 +1,49 @@
+/**
+ * Regex based filter to get cookie values
+ * This includes the =
+ *
+ * @param name { string } - Filter keyword
+ */
+const getCookiePattern = (name: string): RegExp => RegExp(name + '=.[^;]*')
+
+/**
+ * Get cookie from document (Local DOM)
+ *
+ * @param name { string } - Cookie name
+ */
+function getCookieFromDocument(name: string): string | undefined {
+  const pattern = getCookiePattern(name)
+  const matched = document.cookie.match(pattern)
+
+  if (!matched) {
+    return undefined
+  }
+
+  const cookie = matched[0].split('=')
+  return decodeURIComponent(cookie[1])
+}
+
+/**
+ * Set new cookie
+ *
+ * @param name { string } - Cookie name
+ * @param value { string } - Cookie value
+ */
+function setCookie(name: string, value: string): void {
+  document.cookie = name + '=' + value + ';path=/'
+}
+
+/**
+ * Check if the DOM is available
+ * if this function returns false, it means that the page is not rendered by the client.
+ * However, it might be pre-rendered for example on a server.
+ */
+function canUseDOM(): boolean {
+  return !!(
+    typeof window != 'undefined' &&
+    window.document &&
+    window.document.createElement
+  )
+}
+
+export { getCookieFromDocument, setCookie, canUseDOM }
